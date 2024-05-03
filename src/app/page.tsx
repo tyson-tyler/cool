31bc0145e5b150cdfbb464542de95a77597e51f8
tyson-tyler <benword25@gmail.com>
2024-05-03 16:47:39 +0530

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import getTreadingVideo from "@/actions/NewCreator";
+import getCurrentSubscription from "@/actions/getCurrentSubscriptions";
+import getTreadingVideo from "@/actions/getTreadingVideo";
 import LeftBar from "@/components/Leftbar";
 import VideoCard from "@/components/shared/VideoCard";
-import getCurrentSubscription from "@/actions/getCurrentSubscriptions";
 
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: {
-    absolute: "Home",
-  },
-};
 export default async function Home() {
-  const trendingVideos = await getTreadingVideo();
   const subscriptions = await getCurrentSubscription();
+  const trendingVideos = await getTreadingVideo();
   return (
     <div className="w-full relative  mt-16 flex justify-center">
-      <div className="sm:hidden md:flex flex flex-between md:mr-4 fixed z-50 left-0 dark:bg-black bg-white">
+      <div className="sm:hidden md:flex flex flex-between md:mr-4">
         <LeftBar subscribedChannels={subscriptions} />
       </div>
-      <div className="w-full lg:ml-[250px] md:ml-[120px] sm:mb-[100px] lg:mb-[0px] gap-x-10 gap-y-10 mt-9 justify-center grid-container">
+      <div className="basis-[85%] mb-[100px] lg:mb-[0px] gap-x-10 gap-y-10 mt-5 justify-center grid-container">
         {trendingVideos
           ? trendingVideos.map((trendingVideo) => {
               return (
