a064e5ff01960135bdad746f841b49131ab6fb0c
tyson-tyler <benword25@gmail.com>
2024-05-04 18:04:44 +0530

    hello

--- src/actions/getTreadingVideo.ts
+++ src/actions/getTreadingVideo.ts
@@ -1,9 +1,9 @@
-import { useInfiniteQuery } from "react-query";
 import prisma from "@/vendor/db";
 import { Channel, Video } from "@prisma/client";
 
-const fetchTrendingVideos = async (key: any, nextPage = 1) => {
-  const PAGE_SIZE = 10;
+export default async function getTrendingVideos(): Promise<
+  (Video & { channel: Channel })[]
+> {
   try {
     const startDate = new Date();
     startDate.setMonth(startDate.getMonth() - 1);
@@ -22,8 +22,6 @@ const fetchTrendingVideos = async (key: any, nextPage = 1) => {
           viewCount: "desc",
         },
       ],
-      skip: (nextPage - 1) * PAGE_SIZE,
-      take: PAGE_SIZE,
     });
 
     // Add channel information to each video and return as a tuple
@@ -33,8 +31,7 @@ const fetchTrendingVideos = async (key: any, nextPage = 1) => {
     })) as (Video & { channel: Channel })[];
 
     return videosWithChannels;
-  } catch (error) {
-    throw new Error("Failed to fetch trending videos: " + error);
+  } catch (error: any) {
+    throw new Error("Failed to fetch trending videos: " + error.message);
   }
-};
-export default fetchTrendingVideos;
+}
