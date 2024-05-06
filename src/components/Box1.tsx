ee2cf242b9d34f934d7854f142fa35d39bafd549
tyson-tyler <benword25@gmail.com>
2024-05-06 16:41:35 +0530

    daada

--- src/components/Box1.tsx
+++ src/components/Box1.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic
+
+const DynamicImage = dynamic(() => import("next/image")); // Dynamically import next/image component
 
 const BoxCard1 = () => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg border-b hover:border-sky-700 md:mr-5  mr-0 transition hover:scale-105 cursor-pointer">
       <div className="relative w-full h-[300px]">
-        <Image
+        <DynamicImage
           src={"/2.webp"}
           alt="hello"
           layout="fill"
-          objectFit="cover" // Covers the container while maintaining aspect ratio
-          priority // Lazy loading for improved performance
+          objectFit="cover"
+          priority
           quality={75}
         />
       </div>
--- src/components/Box2.tsx
+++ src/components/Box2.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
-import Link from "next/link";
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic
+
+const DynamicImage = dynamic(() => import("next/image")); // Dynamically import next/image component
+const DynamicLink = dynamic(() => import("next/link")); // Dynamically import next/link component
 
 const BoxCard2 = () => {
   return (
-    <Link href={"https://inter-main.vercel.app"}>
+    <DynamicLink href={"https://inter-main.vercel.app"}>
       <div className="max-w-sm rounded overflow-hidden shadow-lg border-b hover:border-sky-700 md:mr-5  mr-0 transition hover:scale-105 cursor-pointer">
         <div className="relative w-full h-[300px]">
-          <Image
+          <DynamicImage
             src={"/3.webp"}
             alt="hello"
             layout="fill"
-            objectFit="cover" // Covers the container while maintaining aspect ratio
-            priority // Lazy loading for improved performance
+            objectFit="cover"
+            priority
             quality={75}
           />
         </div>
@@ -24,7 +26,7 @@ const BoxCard2 = () => {
           </p>
         </div>
       </div>
-    </Link>
+    </DynamicLink>
   );
 };
 
