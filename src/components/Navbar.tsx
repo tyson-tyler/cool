b8d66bccc5d945b4141ccb42f534107ad3ab8f19
tyson-tyler <benword25@gmail.com>
2024-05-24 08:45:44 +0530

    dsfsf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Sidebar from "./sidebar/Sidebar";
 
 export const Navbar = () => {
   return (
-    <nav className="h-16 w-full fixed top-0 z-50 bg-white shadow-lg text-black dark:bg-gray-800 dark:text-white flex justify-between items-center">
+    <nav className="h-16 w-full fixed top-0 z-[60] bg-white shadow-lg text-black dark:bg-gray-800 dark:text-white flex justify-between items-center">
       <Logo />
       <Input />
       <UserOptions />
