d45e4b8100163434859895121c2737b77b43b442
tyson-tyler <benword25@gmail.com>
2024-05-25 19:32:39 +0530

    sfdf

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -61,12 +61,7 @@ const Footer = () => {
     >
       <div className="flex justify-between md:hidden">
         {items.map((item, index) => (
-          <Link
-            href={item.url}
-            className="flex ml-3 mr-3"
-            key={index}
-            prefetch={true}
-          >
+          <Link href={item.url} className="flex ml-3 mr-3" key={index}>
             <div className="flex  items-center gap-x-3  text-2xl my-5 opacity-80 hover:opacity-100 gap-6 transition hover:scale-105">
               <div className="flex">{item.logo}</div>
             </div>
