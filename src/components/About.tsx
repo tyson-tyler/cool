820067652be6e23542d59979c18370389ac6ffb7
tyson-tyler <benword25@gmail.com>
2024-05-26 22:27:19 +0530

    sfsdf

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -56,7 +56,7 @@ const About = async () => {
           </div>
         </section>
       ) : (
-        <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
+        <div className="w-full rounded-md  h-[30rem] overflow-hidden">
           <Vortex
             backgroundColor="black"
             className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
