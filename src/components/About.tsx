b05f80ea08c668c34a8e56fd6bdd19656a0f908e
tyson-tyler <benword25@gmail.com>
2024-05-26 22:22:31 +0530

    sdffsf

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Suspense, lazy } from "react";
 
+import { Vortex } from "./ui/votex";
+import { FlipWords } from "./ui/flip-words";
+
 // export const metadata: Metadata = {
 //   title: {
 //     absolute: "Home",
@@ -15,7 +18,7 @@ import { Suspense, lazy } from "react";
 
 const About = async () => {
   const [isLoading, setIsLoading] = useState(true);
-
+  const words = ["better", "cute", "modern", "real", "cool"];
   useEffect(() => {
     // Simulate data loading delay for 2 seconds
     const timer = setTimeout(() => {
@@ -53,34 +56,43 @@ const About = async () => {
           </div>
         </section>
       ) : (
-        <section className="w-full flex-center flex-col mt-6">
-          <h1 className="head_text text-center pt-[200px] dark:text-white text-black">
-            <span className="dark:text-white text-black">Explore & Create</span>
-            <br className="max-md:hidden" />
-            <span className="usespan text-center ml-2">Ai Video</span>
-          </h1>
+        <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
+          <Vortex
+            backgroundColor="black"
+            className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+          >
+            <section className="w-full flex-center flex-col mt-6">
+              <h1 className="head_text text-center pt-[200px] dark:text-white text-black">
+                <span className="dark:text-white text-black">
+                  Explore <FlipWords words={words} />
+                </span>
+                <br className="max-md:hidden" />
+                <span className="usespan text-center ml-2">Ai Video</span>
+              </h1>
 
-          <p className="desc text-center text-gray-600 mt-5 px-3">
-            Myaimix is an open-source Ai Video & Image watch and create your own
-            .
-          </p>
-          <div className="flex justify-center items-center mt-5 gap-3">
-            <Link href={"/"}>
-              <Button className="p-5 justify-center flex" size={"lg"}>
-                Try it Now
-              </Button>
-            </Link>
-            <a href={"https://inter-main.vercel.app"}>
-              <Button
-                className="p-5 justify-center flex"
-                size={"lg"}
-                variant={"ghost"}
-              >
-                Community
-              </Button>
-            </a>
-          </div>
-        </section>
+              <p className="desc text-center text-gray-600 mt-5 px-3">
+                Myaimix is an open-source Ai Video & Image watch and create your
+                own .
+              </p>
+              <div className="flex justify-center items-center mt-5 gap-3">
+                <Link href={"/"}>
+                  <Button className="p-5 justify-center flex" size={"lg"}>
+                    Try it Now
+                  </Button>
+                </Link>
+                <a href={"https://inter-main.vercel.app"}>
+                  <Button
+                    className="p-5 justify-center flex"
+                    size={"lg"}
+                    variant={"ghost"}
+                  >
+                    Community
+                  </Button>
+                </a>
+              </div>
+            </section>
+          </Vortex>
+        </div>
       )}
     </Suspense>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -63,18 +67,41 @@ const config = {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
       },
       animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), [addVariablesForColors]],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
