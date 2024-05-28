98d68ad93b2a34f4fdebf8eeb2ef9353b6f32a05
tyson-tyler <benword25@gmail.com>
2024-05-28 15:17:36 +0530

    dfsdfsf

--- src/components/videoTrue.tsx
+++ src/components/videoTrue.tsx
@@ -69,7 +69,7 @@ const VideoCard: React.FC<VideoCardProps> = ({
       href={`/video/${video.id}`}
       prefetch={true}
     >
-      <div className="relative w-full flex justify-center lg:justify-center lg:h-[550px] aspect-auto">
+      <div className="relative w-full flex justify-center lg:justify-center lg:h-[550px] aspect-video">
         <video
           ref={videoRef}
           src={video.videoSrc}
